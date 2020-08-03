59efc1de2685d6186ac306773a4871dd17fe5d30
Bibhash <bibhash@umich.edu>
2020-08-03 12:58:52 -0400

    Finalized code.

--- src/main/java/com/eribib/sayhi/HelloFX.java
+++ src/main/java/com/eribib/sayhi/HelloFX.java
@@ -2,24 +2,32 @@ package com.eribib.sayhi;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class HelloFX extends Application {
 
-    @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
+	@Override
+	public void start(Stage primaryStage) {
+		StackPane layout = new StackPane();
+		String javaVersion = System.getProperty("java.version");
+		String javafxVersion = System.getProperty("javafx.version");
+		
+		Button button1 = new Button("Click me");
+		layout.getChildren().add(button1);
+		
+		Label label1 = new Label("Hello, Bibhash " + javafxVersion + ", running on Java " + javaVersion + ".");
+		label1.setMaxWidth(Double.MAX_VALUE);
+		layout.getChildren().add(label1);
+		
+		Scene newscene = new Scene(layout, 300, 250);
+		primaryStage.setScene(newscene);
+		primaryStage.show();
+	}
 
-    public static void main(String[] args) {
-        launch();
-    }
-
+	public static void main(String[] args) {
+		launch();
+	}
 }
